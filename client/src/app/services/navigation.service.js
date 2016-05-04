dd52eb329f7406c78c6702104e71b78eaee0809c
jayanth <cgsteam@MININT-3QUS246>
2016-05-04 18:26:08 -0500

    feat(module-ui): add filter catalog feature

--- client/src/app/services/navigation.service.js
+++ client/src/app/services/navigation.service.js
@@ -8,13 +8,6 @@
     function navigationService() {
         var nav = this;
         nav.values = {};
-        nav.values.displayLoginTab = true;
-        nav.values.displayRegisterTab = true;
-        nav.values.displayMoviesTab = false;
-        nav.values.displaySeriesTab = false;
-        nav.values.displayTopMoviesTab = false;
-        nav.values.displayTopShowsTab = false;
-        nav.displaySearchTab = false;
 
         nav.getValues = getValues;
 
